3e73bcdce4eceed0331cf54007584418d583d3e7
James Johnston <122847695+jamesljohnston77@users.noreply.github.com>
2023-04-30 14:49:44 -0500

    Update Account.cs

--- abc-bank/Account.cs
+++ abc-bank/Account.cs
@@ -1,84 +1,111 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace abc_bank
 {
-    public class Account
+    public class Account : ITransactionData
     {
+        internal readonly IEnumerable<Transaction> transactions;
 
-        public const int CHECKING = 0;
-        public const int SAVINGS = 1;
-        public const int MAXI_SAVINGS = 2;
+        public int Id { get; set; }
+        public int customerId { get; set; }
+        public int accountType { get; set; }
+        public int transactionType { get; set; }
+        public double balance { get; set; }
 
-        private readonly int accountType;
-        public List<Transaction> transactions;
-
-        public Account(int accountType) 
+        public enum AccountType
         {
-            this.accountType = accountType;
-            this.transactions = new List<Transaction>();
+            CHECKING,
+            SAVINGS,
+            MAXI_SAVINGS
+        }
+        public int GetAccountType(AccountType account)
+        {
+            if (account == AccountType.CHECKING) return 0;
+            else if (account == AccountType.SAVINGS) return 1;
+            else return 2;
         }
 
-        public void Deposit(double amount) 
+        //private static readonly Dictionary<string, AccountType> predefinedValues;
+
+        //private readonly int accountType;
+        //public List<Transaction> transactions;
+
+        public Account OpenAccount(Customer customer, int type)
         {
-            if (amount <= 0) {
-                throw new ArgumentException("amount must be greater than zero");
-            } else {
-                transactions.Add(new Transaction(amount));
-            }
+            Account account = new Account();
+            account.customerId = customer.Id;
+            account.accountType = type; 
+            //this.transactions = new List<Transaction>();
+            return account;
         }
 
-        public void Withdraw(double amount) 
-        {
-            if (amount <= 0) {
-                throw new ArgumentException("amount must be greater than zero");
-            } else {
-                transactions.Add(new Transaction(-amount));
-            }
-        }
 
-        public double InterestEarned() 
+        public double InterestEarned(AccountType account) 
         {
-            double amount = sumTransactions();
-            switch(accountType){
-                case SAVINGS:
-                    if (amount <= 1000)
-                        return amount * 0.001;
-                    else
-                        return 1 + (amount-1000) * 0.002;
-    //            case SUPER_SAVINGS:
-    //                if (amount <= 4000)
-    //                    return 20;
-                case MAXI_SAVINGS:
-                    if (amount <= 1000)
-                        return amount * 0.02;
-                    if (amount <= 2000)
-                        return 20 + (amount-1000) * 0.05;
-                    return 70 + (amount-2000) * 0.1;
-                default:
+            double amount = sumTransactions(account);
+            if (account == AccountType.SAVINGS) {
+                if (amount <= 1000)
                     return amount * 0.001;
+                else
+                    return (amount - 1000) * 0.002 + 1000*0.001;
+
+            } 
+            else if (account == AccountType.CHECKING)
+            {
+                     return amount * 0.001;
+            }
+            else
+            {
+                if (amount <= 1000)
+                    return amount * 0.002;
+                else
+                    return (amount - 1000) * 0.005;
             }
         }
 
-        public double sumTransactions() {
-           return CheckIfTransactionsExist(true);
+        public double sumTransactions(AccountType account) {
+           return CheckIfTransactionsExist(customerId,account);
         }
 
-        private double CheckIfTransactionsExist(bool checkAll) 
+        private double CheckIfTransactionsExist(int customerId, AccountType account)
+        {
+            double QSTotal = (from balance in transactions
+                              where balance.customerId == customerId && balance.accountType == (int)account
+                              select balance.amount).Sum();
+            return QSTotal;
+        }
+
+        IEnumerable<Transaction> GetAll(int customerId)
+        {
+            //return transactions;
+            return from r in transactions
+                   where r.customerId == customerId
+                   select r;
+
+        }
+
+        private double CheckIfTransactionsExist(Customer customer, Account account)
         {
             double amount = 0.0;
+            List<Transaction> transactions = null;
+            transactions.Add((Transaction)from r in transactions
+                                          where r.customerId == customerId
+                                          select r);
             foreach (Transaction t in transactions)
                 amount += t.amount;
             return amount;
         }
 
-        public int GetAccountType() 
+        IEnumerable<Transaction> ITransactionData.GetAll()
         {
-            return accountType;
+            return from r in transactions
+                   select r;
+            //throw new NotImplementedException();
         }
-
     }
 }
